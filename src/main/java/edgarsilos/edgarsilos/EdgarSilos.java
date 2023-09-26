fa6a6e6ae9abd61b7671897e4887ee5ae9b573cc
EdgarSilos <edgarsilos2005@gmail.com>
2023-09-26 10:03:04 -0500

    mi primer programa

---
+++ src/main/java/edgarsilos/edgarsilos/EdgarSilos.java
@@ -0,0 +1,9 @@
+package edgarsilos.edgarsilos;
+public class EdgarSilos {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Edgar Alan Santiago Silos");
+        
+    }
+}
